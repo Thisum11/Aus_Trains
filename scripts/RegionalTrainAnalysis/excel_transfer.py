189c2ab674fe57f77093c6579fcfa24f0530c7f9
Thisum11 <147669362+Thisum11@users.noreply.github.com>
2025-04-19 00:41:58 +0930

    Importing the Processed data files into python

---
+++ scripts/RegionalTrainAnalysis/excel_transfer.py
@@ -0,0 +1,6 @@
+import pandas as pd
+df = pd.read_excel("C:\Academic\Aus_Trains\data\Processed_Data\RailwayInfo.xlsx")
+# Importing the excel file to python
+
+print(df.head())
+#Making sure the file is successfully imported.
