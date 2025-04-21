8fc3c63b22dc87f16a09cb5d79497c4a1c2559d8
Thisum11 <147669362+Thisum11@users.noreply.github.com>
2025-04-21 20:43:46 +0930

    Creating the location markers

--- scripts/RegionalTrainAnalysis/Station_map.py
+++ scripts/RegionalTrainAnalysis/Station_map.py
@@ -7,4 +7,11 @@ df = pd.read_excel(r"C:\Academic\Aus_Trains\data\Processed_Data\RailwayInfo.xlsx
 m = folium.Map(location=[-25.2744, 133.7751], zoom_start =5)
 # This creates the Australian Map
 
+for index, row in df.iterrows():
+    folium.Marker(
+        location=[row['Latitude'], row['Longitude']],
+        popup=row['Station_name'],
+        tooltip=row['Station_name'],
+    ).add_to(m)
+# Creation of the markers in the given railway station locations
 m.save("Australian_Railway_Stations.html")
